6e1f87e8faf47e33162bae85149f4f35cf9d86e3
kalle (jag) <kalle.jillheden@me.com>
2017-10-02 15:57:28 +0200

    bash door

--- Items/Dungeon/ItemTorch.cs
+++ Items/Dungeon/ItemTorch.cs
@@ -1,4 +1,6 @@
-﻿namespace Zork_Grupp_L.Items
+﻿using System;
+
+namespace Zork_Grupp_L.Items
 {
 	using Zork_Grupp_L.Helpers;
     using Zork_Grupp_L.Items.Dungeon;
@@ -26,14 +28,28 @@
 
 			if (otherItem is ExitBurnDownableDoor door)
 		    {
-			    ConsoleHelper.WriteLineWrap("You burnt down that door ayao, cool.");
-			    // Remove door from it's parent inventory
-			    door.CurrentInventory.InventoryRemoveItem(door);
-			    // Add new (and not locked) door to room
-			    Game.CurrentRoom.AddRoomExit(door.NextRoom,
-				    name: "burnt down door",
-				    description: "This is a really burnt da0wn door. It seem to have been locked, but not anymore, fam.");
+			    if (this.IsLit)
+			    {
+				    ConsoleHelper.WriteLineWrap("You burnt down that door ayao, cool.");
+				    // Remove door from it's parent inventory
+				    door.CurrentInventory.InventoryRemoveItem(door);
+				    // Add new (and not locked) door to room
+				    Game.CurrentRoom.AddRoomExit(door.NextRoom,
+					    name: "burnt down door",
+					    description: "This is a really burnt da0wn door. It seem to have been locked, but not anymore, fam.");
+			    }
+			    else
+			    {
+				    Console.ForegroundColor = Colors.ErrorColor;
+				    ConsoleHelper.WriteLineWrap("The torch is not burning, but in your frustration you bash the door open with the {0}.", this.Name);
 
+				    // Remove door from it's parent inventory
+				    door.CurrentInventory.InventoryRemoveItem(door);
+				    // Add new (and not locked) door to room
+				    Game.CurrentRoom.AddRoomExit(door.NextRoom,
+					    name: "broken door",
+					    description: "A collection of smitherines on the floor. It seem to have been locked, but you can just walk over the crumbs.");
+				}
 			    return true;
 		    }
 
